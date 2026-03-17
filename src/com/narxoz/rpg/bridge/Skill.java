5095791e5b09b5cea2ec15a55acd230b1a3920f0
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-18 00:40:12 +0500

    mplement Skill class to utilize EffectImplementor for damage application

--- src/com/narxoz/rpg/bridge/Skill.java
+++ src/com/narxoz/rpg/bridge/Skill.java
@@ -1,5 +1,14 @@
 package com.narxoz.rpg.bridge;
 
 public class Skill {
-    
+
+    private EffectImplementor effect;
+
+    public Skill(EffectImplementor effect) {
+        this.effect = effect;
+    }
+
+    public int use(int baseDamage) {
+        return effect.applyEffect(baseDamage);
+    }
 }
